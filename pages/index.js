e4d0bf60baac77a1e5c5d08c9015017c1db9b97b
aluis <andreluis9214@gmail.com>
2017-11-04 17:43:24 -0200

    feat: Add users page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Router from 'next/router';
+
 import MainLayout from '../layouts/Main';
 import SearchForm from '../components/modules/SearchForm';
 
 export default () => (
     <MainLayout title="- Home">
-        <SearchForm onSubmit={values => console.log('values', values)} />
+        <SearchForm
+            onSubmit={({ user }) => {
+                Router.push(`/users?username=${user}`, `/users/${user}`);
+            }}
+        />
     </MainLayout>
 );
---
+++ pages/users/index.js
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import { getUserInfo } from '../../services/githubAPI';
+
+import Avatar from '../../components/elements/Avatar';
+import InfoListItem from '../../components/elements/InfoListItem';
+
+export default class MyPage extends Component {
+    static async getInitialProps(context) {
+        const { username } = context.query;
+
+        const res = await getUserInfo(username);
+        const userInfo = await res.json();
+        return { userInfo };
+    }
+
+    render() {
+        const {
+            userInfo,
+        } = this.props;
+
+        return (
+            <div>
+                <Avatar url={userInfo.avatar_url} />
+
+                <ul>
+                    <InfoListItem
+                        title="Username:"
+                        description={userInfo.login}
+                        url={userInfo.htmlUrl}
+                    />
+
+                    <InfoListItem
+                        title="Followers:"
+                        description={userInfo.followers}
+                        url={userInfo.followers_url}
+                    />
+
+                    <InfoListItem
+                        title="Following:"
+                        description={userInfo.following}
+                        url={userInfo.following_url}
+                    />
+
+                    <InfoListItem
+                        title="Bio:"
+                        description={userInfo.bio}
+                    />
+
+                    <InfoListItem
+                        title="email:"
+                        description={userInfo.email}
+                    />
+                </ul>
+            </div>
+        );
+    }
+}
