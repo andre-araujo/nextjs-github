92555fb50fc3d8e2c11c481dd99681702b03effc
aluis <andreluis9214@gmail.com>
2017-11-06 22:09:03 -0200

    test: Add Submit component missing tests

--- components/elements/Submit/index.spec.js
+++ components/elements/Submit/index.spec.js
@@ -4,16 +4,15 @@ import { shallow } from 'enzyme';
 import Submit from './index';
 
 describe('Submit component', () => {
-    it('should render without crashing', () => {
-        shallow(<Submit />);
+    it('should match snapshot with value as testing', () => {
+        expect(shallow(
+            <Submit>testing</Submit>,
+        )).toMatchSnapshot();
     });
 
-    it('should have a value in sync with children content', () => {
-        const wrapper = shallow(<Submit>testing</Submit>);
-
-        expect(
-            wrapper.find('input').contains(
-                <input type="submit" className="submit" value={'testing'} />,
-            )).toBeTruthy();
+    it('should match snapshot with value as tests', () => {
+        expect(shallow(
+            <Submit>tests</Submit>,
+        )).toMatchSnapshot();
     });
 });
